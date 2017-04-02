474ba6f293337fd41270bf344a936f565d9c70bf
github username <github email address>
2017-04-02 16:39:16 -0400

    Done.

--- components/Product.js
+++ components/Product.js
@@ -1,14 +1,16 @@
-const React = require('react')
+const React = require('react');
 
 class Product extends React.Component {
   render () {
-    <div>
-      <p>Name: {this.props.name}</p>
-      <p>Producer: {this.props.producer}</p>
-      <p>Watermark?: {this.props.hasWatermark}</p>
-      <p>Color: {this.props.color}</p>
-      <p>Weight: {this.props.weight}</p>
-
+    return (
+      <div className="product">
+        <p>Name: {this.props.name}</p>
+        <p>Producer: {this.props.producer}</p>
+        <p>Watermark?: {this.props.hasWatermark}</p>
+        <p>Color: {this.props.color}</p>
+        <p>Weight: {this.props.weight}</p>
+      </div>
+    )
   }
 }
 
@@ -20,9 +22,14 @@ Product.propTypes = {
   name: React.PropTypes.string.isRequired,
   producer: React.PropTypes.string,
   hasWatermark: React.PropTypes.bool,
-  color: React.PropTypes.string.isRequired,
-  weight: React.PropTypes.number.isRequired,
+  color: React.PropTypes.oneOf(['white', 'salmon', 'eggshell-white']).isRequired,
+  weight: (props, name) => {
+    const weight = props[name]
+    if (weight == undefined) new Error ('Enter weight')
+    if (isNan(weight)) new Error ('Enter weight')
+    if (weight < 80 || weight > 300) new Error ('80 - 300 only')
+  }
 
 }
 
-module.export = Product;
+module.exports = Product;
